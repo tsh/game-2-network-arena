0b164a070dd9f43d5e64ba47f5661a1af69359cd
annaFalse <anna.latysheva13@gmail.com>
2018-03-11 11:41:03 +0200

    dummy server

--- server/server.py
+++ server/server.py
@@ -1,13 +1,25 @@
 
+import asyncio
 
 class BaseServer(object):
     def __init__(self):
         print('Hello init')
 
-    async def run(self):
+    async def run(self, reader, writer):
         print('async world')
-
+        while True:
+            print('Test')
 if __name__ == '__main__':
     print('hello')
     srv = BaseServer()
-    srv.run()
+    event_loop = asyncio.get_event_loop()
+    factory = asyncio.start_server(srv.run, 'localhost', 8000)
+    server = event_loop.run_until_complete(factory)
+    try:
+        event_loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.close()
+        event_loop.run_until_complete(server.wait_closed())
+        event_loop.close()
