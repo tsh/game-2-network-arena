85a4f7dccb311c031cb40bb7e2f6f4ad3806747e
tsh <dr.tallin@gmail.com>
2018-03-24 17:13:50 +0200

    write current number to client

--- server/server.py
+++ server/server.py
@@ -1,21 +1,38 @@
 
 import asyncio
 
-class BaseServer(object):
-    counter = 0
-    def __init__(self):
-        print('Hello init')
+ADDRESS = '127.0.0.1'
+PORT = 8888
+
+class Server:
+    connections = []
+
+    def __init__(self, world):
+        self.world = world
 
     async def run(self):
-        print('async world')
+        await self.world.run()
+
+    async def connect(self, r, writer):
+        print('Server: Got connection from: {}'.format(writer.get_extra_info('peername')))
+        while True:
+            writer.write(bytes(str(self.world.counter), 'utf8'))
+            await writer.drain()
+            await asyncio.sleep(2)
+
+
+class ClassGameWorld(object):
+    counter = 0
+
+    async def run(self):
         while True:
             self.counter += 1
             await asyncio.sleep(1)
-            print('slept')
+            print('WORLD: counter={}'.format(self.counter))
 
 
 async def client_handler():
-    reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
+    reader, writer = await asyncio.open_connection(ADDRESS, PORT)
     while True:
         data = await reader.read(100)
         print('Client Received: %r' % data.decode())
@@ -30,11 +47,13 @@ async def server_handler(reader, writer):
 
 
 if __name__ == '__main__':
+    world = ClassGameWorld()
+    my_server = Server(world)
     loop = asyncio.get_event_loop()
-    coro = asyncio.start_server(server_handler, '127.0.0.1', 8888, loop=loop)
+    coro = asyncio.start_server(my_server.connect, ADDRESS, PORT)
     server = loop.run_until_complete(coro)
-    loop.run_until_complete(BaseServer().run())
-    loop.run_until_complete(client_handler())
+    asyncio.async(my_server.run())
+    asyncio.async(client_handler())
 
     try:
         loop.run_forever()
