2230aac6717df756b8536d2b012e7d0c2aa7d107
tsh <dr.tallin@gmail.com>
2018-04-11 22:42:26 +0300

    fix big tile

--- client/map.py
+++ client/map.py
@@ -10,24 +10,25 @@ class Map:
         self.MAX_TILES_HORIZ = 0
         self.MAX_TILES_VERT = 0
 
-        self.tile_sprite = pygame.image.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets', 'tiles', 'grass.png'))
-        self.tile_sprite.convert()
+        self.tile_grass = pygame.image.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets', 'tiles', 'grass.png'))
+        self.tile_grass = pygame.transform.scale(self.tile_grass, (self.TILE_SIZE, self.TILE_SIZE))
+        self.tile_grass.convert()
 
     def initialize(self, map_array):
         self.map = map_array
-        self.MAX_TILES_HORIZ = len(self.map[0]) +2
-        self.MAX_TILES_VERT = len(self.map) +2
+        self.MAX_TILES_HORIZ = len(self.map[0])
+        self.MAX_TILES_VERT = len(self.map)
 
     def render(self, window_srf):
         map_srf = pygame.Surface((self.TILE_SIZE * self.MAX_TILES_HORIZ, self.TILE_SIZE * self.MAX_TILES_VERT))
         map_srf.fill((255, 255, 255))
         for i, row in enumerate(self.map):
             for j, tile in enumerate(row):
+                r = pygame.Rect(j * self.TILE_SIZE, i * self.TILE_SIZE, self.TILE_SIZE, self.TILE_SIZE)
                 if tile == 1:
-                    obj = self.tile_sprite
-                    map_srf.blit(obj, (j * self.TILE_SIZE, i * self.TILE_SIZE))
+                    obj = self.tile_grass
+                    map_srf.blit(obj, r)
                 else:
-                    r = pygame.Rect(i * self.TILE_SIZE, j * self.TILE_SIZE, self.TILE_SIZE, self.TILE_SIZE)
                     map_srf.fill(Color('blue'), r)
                 window_srf.blit(map_srf, (0, 0))
                 pygame.display.flip()
