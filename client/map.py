8a98a2e06f0b2ffce2976175b69d9dc0e82c8787
tsh <dr.tallin@gmail.com>
2018-05-09 16:26:52 +0300

    rename

--- client/map.py
+++ client/map.py
@@ -7,7 +7,7 @@ class Map:
     def __init__(self):
         self.map = []
         self.TILE_SIZE = 64
-        self.MAX_TILES_HORIZ = 0
+        self.MAX_TILES_HORZ = 0
         self.MAX_TILES_VERT = 0
 
         self.tile_grass = pygame.image.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'assets', 'tiles', 'grass.png'))
@@ -16,11 +16,11 @@ class Map:
 
     def initialize(self, map_array):
         self.map = map_array
-        self.MAX_TILES_HORIZ = len(self.map[0])
+        self.MAX_TILES_HORZ = len(self.map[0])
         self.MAX_TILES_VERT = len(self.map)
 
     def render(self, window_srf):
-        map_srf = pygame.Surface((self.TILE_SIZE * self.MAX_TILES_HORIZ, self.TILE_SIZE * self.MAX_TILES_VERT))
+        map_srf = pygame.Surface((self.TILE_SIZE * self.MAX_TILES_HORZ, self.TILE_SIZE * self.MAX_TILES_VERT))
         map_srf.fill((255, 255, 255))
         for i, row in enumerate(self.map):
             for j, tile in enumerate(row):
